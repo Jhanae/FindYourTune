20f9b6e9ba00afe0a03d8e55390b197310c4f973
Your name <you@example.com>
2021-09-09 20:49:12 -0400

    Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {BrowserRouter as Router, Switch,Route} from "react-router-dom"
+import Login from './Login'
 function App() {
   return (
     <div className="App">
-      <h1>HEYYYY!!!!!!!</h1>
-    </div>
+      <Router>
+          <Switch>
+          <Route exact path="/" component={() => <Login />} />
+          {/* <Route exact path="/shop" component={() => <ProductContainer products={products} setProductID={setProductID} productID={productID} setprofileID={setProfileID} profileID={profileID}/>} />
+          <Route exact path="/cart" component={() => <Cart profileID={profileID} />} />
+          <Route exact path="/log-in" component={() => <LogIn isLoggedIn={isLoggedIn} setisLoggedIn={setisLoggedIn} setProfileID={setProfileID} profileID={profileID}/>} />
+          <Route exact path="/profile" component={() => <Profile isLoggedIn={isLoggedIn} profileID={profileID}/>} />
+          <Route exact path="/update-profile" component={() => <EditProfile profileID={profileID} isLoggedIn={isLoggedIn}/>} /> */}
+         </Switch>
+      </Router>
+      </div>
   );
 }
 
---
+++ client/src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Login() {
+    return (
+        <div className="loginBackground">
+            <div className="row">
+            <div className="col-sm" >
+                 <br/><br/><br/>
+                <h1>Welcome Back!</h1>
+            </div>
+            <div className="col-sm">
+                <h1>HEFdk</h1>
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
